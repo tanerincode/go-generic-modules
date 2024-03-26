214102c161a8bdbd4921b25353cc995c0f9bf47c
tanerincode <tombastaner@gmail.com>
2024-03-26 13:46:56 +0300

    adding config and default health check service

---
+++ pkg/service/health_check_service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"net/http"
+	responsehandler "tanerincode/go-generic-modules/pkg/responseHandler"
+	"tanerincode/go-generic-modules/pkg/storage"
+)
+
+type HealthCheckService interface {
+	Check(w http.ResponseWriter)
+}
+
+type healthCheckService struct {
+	db storage.Storage
+}
+
+func (s *healthCheckService) Check(w http.ResponseWriter) {
+	err := s.db.Disconnect()
+	if err != nil {
+		response := &responsehandler.Response{
+			Status:  false,
+			Code:    500,
+			Message: "Database connection failed",
+		}
+		responsehandler.RespondAsJSON(w, response, err)
+		return
+	}
+
+	response := &responsehandler.Response{
+		Status:  true,
+		Code:    200,
+		Message: "Database connection successful",
+	}
+	responsehandler.RespondAsJSON(w, response, nil)
+}
+
+func NewHealthCheckService(db storage.Storage) HealthCheckService {
+	return &healthCheckService{db: db}
+}
